cec10ce78375043a37806dae9d1fd77893730f0f
Maggie-cahill <114490169+Maggie-cahill@users.noreply.github.com>
2024-04-11 22:55:56 +0100

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,60 @@
+let shopItemsData =[{
+    id: "brownHoodie",
+    name: "Brown Hoodie",
+    price: 45,
+    img: "Images/Shop/BrownHoodie.png"
+},
+{
+    id: "blackHoodie",
+    name: "Blue Hoodie",
+    price: 45,
+    img: "Images/Shop/Hoodie.png" 
+},
+{
+    id: "greenHoodie",
+    name: "Green Hoodie",
+    price: 45,
+    img: "Images/Shop/GreenHoodie.png"
+},
+{
+    id: "blackSweatshirt",
+    name: "Black Sweatshirt",
+    price: 40,
+    img: "Images/Shop/Sweatshirt.png"
+},
+{
+    id: "redSweatshirt",
+    name: "Red Sweatshirt",
+    price: 40,
+    img: "Images/Shop/RedSweatshirt.png"
+},
+{
+    id: "pinkShirt",
+    name: "Styled T",
+    price: 25,
+    img: "Images/Shop/Shirt.jpg"
+},
+{
+    id: "beanie",
+    name: "Beanie",
+    price: 20,
+    img: "Images/Shop/Beanie.png"
+},
+{
+    id: "pinkHat",
+    name: "Pink Cap",
+    price: 15,
+    img: "Images/Shop/Hat.png"
+},
+{
+    id: "blueHat",
+    name: "Blue Cap",
+    price: 15,
+    img: "Images/Shop/BlueHat.png"
+},
+{
+    id: "shoes",
+    name: "OG Shoes",
+    price: 75,
+    img: "Images/Shop/Shoes.png"
+}];
